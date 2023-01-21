986f6ba3205787219281ad63b30b6a51dd24215a
dudubfjj <dudubfjj@gmail.com>
2023-01-21 12:03:00 -0300

    response

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ async def get_url(ctx, id_gc):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
     response = requests.get(f'https://gamersclub.com.br/api/box/init/{322861}', cookies=cookies, headers=headers).json()
+    await ctx.send(response)
    
     estatisticas = response['stats']
 
